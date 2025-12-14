2ec6dac7129fd2d139410fbeb045f596c6b3051a
Jansaida471 <jansaida471@gmail.com>
2025-12-14 12:03:18 +0530

    hostadded

--- aiven_deploy/aiven_deploy/settings.py
+++ aiven_deploy/aiven_deploy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-project-avine-deploye-purpose.onrender.com"]
 
 
 # Application definition
